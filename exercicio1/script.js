0bd2c729994469ad32f8cf6f3387e613ae882184
FehDx01 <lessa.fernando01@gmail.com>
2023-11-12 15:08:33 -0300

    exercicio-operadores

---
+++ exercicio1/script.js
@@ -0,0 +1,16 @@
+const numero = Number(prompt("Informe um número par"));
+const divisao = 2
+
+const resultado = numero%divisao
+
+console.log(resultado)
+
+// Assim como na matemática, ao inserir números par, não haverá resto = 0
+// Inserindo números impares, haverá restos
+
+
+
+
+
+
+
---
+++ exercicio2/script.js
@@ -0,0 +1,21 @@
+//# Exercício 2
+
+//Faça um programa que pergunte ao usuário dois números.
+
+const num1 = Number(prompt("Diga um número"))
+const num2 = Number(prompt("Diga outro número"))
+
+console.log("O primeiro número é maior que o segundo?", num1 > num2);
+console.log("O primeiro número é igual ao segundo?", num1 === num2);
+console.log("O primeiro numero é divisivel pelo segundo?", (num1 % num2)===0);
+console.log("O segundo numero é divisivel pelo primeiro?", (num2 % num1)===0);
+
+
+
+
+
+
+
+
+
+
